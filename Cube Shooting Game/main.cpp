bf20632fa3357ed8c3440dc9d8ff047f3592d9f0
Alex Morales Garcia <alex2521999@github.com>
2018-03-02 16:53:49 +0100

    +change code & edit README

--- Cube Shooting Game/main.cpp
+++ Cube Shooting Game/main.cpp
@@ -1,13 +1,55 @@
 #include "../SDL/include/SDL.h"
-#include <iostream>
 
 #pragma comment(lib, "../SDL/libx86/SDL2.lib")
 #pragma comment(lib, "../SDL/libx86/SDL2main.lib")
 
-using namespace std;
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
 
 int main(int argc, char* argv[]) {
-	cout << "Hello World" << endl;
-	system("pause");
+	//Initialize window and renderer
+	SDL_Window* window = NULL;
+	SDL_Renderer* renderer = NULL;
+
+	//Initialize SDL
+	SDL_Init(SDL_INIT_VIDEO);
+
+	//Create window
+	window = SDL_CreateWindow("Shooting Cube Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+
+	//Define Red Quad
+	SDL_Rect RedQuad;
+	RedQuad.x = 0;
+	RedQuad.y = 0;
+	RedQuad.w = 80;
+	RedQuad.h = 80;
+
+	//Define Green Quad
+	SDL_Rect GreenQuad;
+	GreenQuad.x = (RedQuad.x) + 80;
+	GreenQuad.y = (RedQuad.y) + 30;
+	GreenQuad.w = 30;
+	GreenQuad.h = 10;
+
+	//Print blue blackground
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	bool loop = true;
+
+	while(loop) {
+		SDL_Event e;
+		if (SDL_PollEvent(&e) != 0) {
+			switch (e.type) {
+			case SDL_QUIT:
+				loop = false;
+			}
+		}
+			
+	}
+
 	return 0;
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Shooting Cube
+## v0.1
++configure SDL libraries
++add "Hello World Code"
+
+## v0.2
+-remove "Hello World Code"
++add "Print Blue Screen Code"
++add quit option
