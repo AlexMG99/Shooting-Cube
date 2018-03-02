0d18efb00a877cf4c165bd10631608c2766ee525
Alex Morales Garcia <alex2521999@github.com>
2018-03-02 16:07:16 +0100

    +add Visual Studio Code Folder

---
+++ Cube Shooting Game/main.cpp
@@ -0,0 +1,13 @@
+#include "../SDL/include/SDL.h"
+#include <iostream>
+
+#pragma comment(lib, "../SDL/libx86/SDL2.lib")
+#pragma comment(lib, "../SDL/libx86/SDL2main.lib")
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout << "Hello World" << endl;
+	system("pause");
+	return 0;
+}
