43f2f3861020a428c5d4adfcd3565a624554fade
Alex Morales Garcia <alex2521999@github.com>
2018-03-06 18:37:33 +0100

    Added New Folders
    SML_Mixer & SDL_Image

--- Cube Shooting Game/Cube Shooting Game/main.cpp
+++ Cube Shooting Game/Cube Shooting Game/main.cpp
@@ -1,10 +1,12 @@
 #include "../SDL/include/SDL.h"
 #include "../SDL_Image/include/SDL_image.h"
+#include "../SDL_Mixer/include/SDL_mixer.h"
 #include <iostream>
 
 #pragma comment(lib, "../SDL/libx86/SDL2.lib")
 #pragma comment(lib, "../SDL/libx86/SDL2main.lib")
 #pragma comment(lib, "../SDL_Image/libx86/SDL2_image.lib")
+#pragma comment(lib, "../SDL_Mixer/libx86/SDL2_mixer.lib")
 
 //Define
 
@@ -38,7 +40,6 @@ int main(int argc, char* argv[]) {
 	bool moveLeft = false;
 	bool bulletExist = false;
 
-
 	//Initialize SDL
 	SDL_Init(SDL_INIT_VIDEO);
 	IMG_Init(IMG_INIT_PNG);
@@ -48,20 +49,20 @@ int main(int argc, char* argv[]) {
 	renderer = SDL_CreateRenderer(window, -1, 0);
 
 	//Define Player Surface
-	SDL_Rect* RedQuad = new SDL_Rect;
-	RedQuad->x = 0;
-	RedQuad->y = 0;
-	RedQuad->w = 123;
-	RedQuad->h = 60;
+	SDL_Rect* Ship_Surface = new SDL_Rect;
+	Ship_Surface->x = 0;
+	Ship_Surface->y = 0;
+	Ship_Surface->w = 123;
+	Ship_Surface->h = 60;
 
 	//Define Bullet Surface
-	SDL_Rect * Bullet[5];
+	SDL_Rect * Bullet_Surface[5];
 	for (int a = 0; a < 5; a++) {
-		Bullet[a] = new SDL_Rect;
-		Bullet[a]->x;
-		Bullet[a]->y;
-		Bullet[a]->w = 30;
-		Bullet[a]->h = 30;
+		Bullet_Surface[a] = new SDL_Rect;
+		Bullet_Surface[a]->x;
+		Bullet_Surface[a]->y;
+		Bullet_Surface[a]->w = 30;
+		Bullet_Surface[a]->h = 30;
 	}
 
 	//Load Textures
@@ -72,100 +73,111 @@ int main(int argc, char* argv[]) {
 	while (loop) {
 		SDL_Event e;
 		if (SDL_PollEvent(&e) != 0) {
-				if (e.type == SDL_QUIT){ //Quit program
+			if (e.type == SDL_QUIT) { //Quit program
+				loop = false;
+			}
+			if (e.type == SDL_KEYDOWN) {//Movement & Attack
+				if (e.key.keysym.sym == SDLK_UP) {
+					moveUp = true;
+				}
+				if (e.key.keysym.sym == SDLK_DOWN) {
+					moveDown = true;
+				}
+				if (e.key.keysym.sym == SDLK_LEFT) {
+					moveLeft = true;
+				}
+				if (e.key.keysym.sym == SDLK_RIGHT) {
+					moveRight = true;
+				}
+				if (e.key.keysym.sym == SDLK_SPACE) {
+					bulletExist = true;
+				}
+				if (e.key.keysym.sym == SDLK_ESCAPE) {
 					loop = false;
 				}
-				if (e.type == SDL_KEYDOWN) {//Movement & Attack
-					if (e.key.keysym.sym == SDLK_UP) {
-						moveUp = true;
-					}
-					if (e.key.keysym.sym == SDLK_DOWN) {
-						moveDown = true;
-					}
-					if (e.key.keysym.sym == SDLK_LEFT) {
-						moveLeft = true;
-					}
-					if (e.key.keysym.sym == SDLK_RIGHT) {
-						moveRight = true;
-					}
-					if (e.key.keysym.sym == SDLK_SPACE) {
-							Bullet[numBullet]->x = (RedQuad->x) + (RedQuad->w);
-							Bullet[numBullet]->y = (RedQuad->y)+((RedQuad->h)/2 - Bullet[1]->h);
-							numBullet++;
-							if (numBullet == 5) {
-								numBullet = 0;
-							}
-					}
-						if (e.key.keysym.sym == SDLK_ESCAPE) {
-							loop = false;
-						}
-					}
-				if (e.type == SDL_KEYUP) {
-					if (e.key.keysym.sym == SDLK_UP) {
-						moveUp = false;
-					}
-					if (e.key.keysym.sym == SDLK_DOWN) {
-						moveDown = false;
-					}
-					if (e.key.keysym.sym == SDLK_LEFT) {
-						moveLeft = false;
-					}
-					if (e.key.keysym.sym == SDLK_RIGHT) {
-						moveRight = false;
-					}
+			}
+
+			if (e.type == SDL_KEYUP) {
+				if (e.key.keysym.sym == SDLK_UP) {
+					moveUp = false;
 				}
+				if (e.key.keysym.sym == SDLK_DOWN) {
+					moveDown = false;
 				}
+				if (e.key.keysym.sym == SDLK_LEFT) {
+					moveLeft = false;
+				}
+				if (e.key.keysym.sym == SDLK_RIGHT) {
+					moveRight = false;
+				}
+				if (e.key.keysym.sym == SDLK_SPACE) {
+					bulletExist = false;
+				}
+			}
+		}
 				
 		
 
 		//Set Borders
-		if (RedQuad->x < 0) {
-			RedQuad->x = 0;
+		if (Ship_Surface->x < 0) {
+			Ship_Surface->x = 0;
 		}
-		if (RedQuad->x > WINDOW_WIDTH-RedQuad->w ) {
-			RedQuad->x = WINDOW_WIDTH - RedQuad->w;
+		if (Ship_Surface->x > WINDOW_WIDTH - Ship_Surface->w ) {
+			Ship_Surface->x = WINDOW_WIDTH - Ship_Surface->w;
 		}
-		if (RedQuad->y < 0) {
-			RedQuad->y = 0;
+		if (Ship_Surface->y < 0) {
+			Ship_Surface->y = 0;
 		}
-		if (RedQuad->y > WINDOW_HEIGHT-RedQuad->h) {
-			RedQuad->y = WINDOW_HEIGHT - RedQuad->h;
+		if (Ship_Surface->y > WINDOW_HEIGHT-Ship_Surface->h) {
+			Ship_Surface->y = WINDOW_HEIGHT - Ship_Surface->h;
 		}
 
 		SDL_RenderCopy(renderer, background, 0, 0);
-		SDL_RenderCopy(renderer, ship, 0, RedQuad);
+		SDL_RenderCopy(renderer, ship, 0, Ship_Surface);
 
 		
 
-		//Print Green Quad
+		//Print Bullet
 		for (int a = 0; a < 5; a++) {
-			SDL_RenderCopy(renderer, bullet, 0, Bullet[a]);
-			Bullet[a]->x += 10;
+			SDL_RenderCopy(renderer, bullet, 0, Bullet_Surface[a]);
+			Bullet_Surface[a]->x += 10;
+		}
+
+		//Bullet 
+		if (bulletExist) {
+			Bullet_Surface[numBullet]->x = (Ship_Surface->x) + (Ship_Surface->w);
+			Bullet_Surface[numBullet]->y = (Ship_Surface->y) + ((Ship_Surface->h) / 2 - Bullet_Surface[1]->h);
+			numBullet++;
+			if (numBullet == 5) {
+				numBullet = 0;
+			}
+			bulletExist = false;
 		}
 
 		//Movement
 		if (moveUp == true) {
-			RedQuad->y -= 5;
+			Ship_Surface->y -= 5;
 		}
 		if (moveDown == true) {
-			RedQuad->y += 5;
+			Ship_Surface->y += 5;
 		}
 		if (moveLeft == true) {
-			RedQuad->x -= 5;
+			Ship_Surface->x -= 5;
 		}
 		if (moveRight == true) {
-			RedQuad->x += 5;
+			Ship_Surface->x += 5;
 		}
 
-	
-		
 		SDL_RenderPresent(renderer);
 		SDL_Delay(10);
 	}
 
+	SDL_DestroyTexture(background);
+	SDL_DestroyTexture(bullet);
+	SDL_DestroyTexture(ship);
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	IMG_Quit();
 	SDL_Quit();
 	return 0;
-
 }
